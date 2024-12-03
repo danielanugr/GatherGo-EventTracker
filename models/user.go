687db9519cec8056f1101142426dfeea8096ca56
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2024-12-03 22:40:30 +0800

    implement user controller and service
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 type User struct {
+	Id          string `json:"id" bson:"_id"`
 	Name        string `json:"name" bson:"user_name"`
 	Email       string `json:"email" bson:"user_email"`
 	PhoneNumber int    `json:"phone_number" bson:"user_phone_number"`
--- services/user.service.go
+++ services/user.service.go
@@ -5,7 +5,7 @@ import "github.com/danielanugr/GatherGo-EventTracker/models"
 type UserService interface {
 	CreateUser(*models.User) error
 	GetUserById(*string) (*models.User, error)
-	GetAll() (*[]models.User, error)
+	GetAll() ([]*models.User, error)
 	UpdateUser(*models.User) error
 	DeleteUser(*string) error
 }
